6318ace4ad1884ae60c1e5a0228199e79fc290f8
chadders <matthew-github@chadders.org>
2025-01-14 19:55:38 +1100

    08_geometry_rendering

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ OBJS = src/main.c
 CC = gcc
 COMPILER_FLAGS = -w
 LINKER_FLAGS = -lSDL2 -lSDL2_image
-OBJ_NAME = exercise_07
+OBJ_NAME = exercise_08
 
 all: $(OBJS)
 	$(CC) $(OBJS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME)
--- src/main.c
+++ src/main.c
@@ -190,6 +190,7 @@ int main( int argc, char* args[] )
 
         while( !quit )
         {
+
             while( SDL_PollEvent( &e ) != 0 )
             {
                 if( e.type == SDL_QUIT )
@@ -228,8 +229,37 @@ int main( int argc, char* args[] )
             }
 
             // Update the surface
+            SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
             SDL_RenderClear( gRenderer );
+
+            // Add image
             SDL_RenderCopy( gRenderer, gTexture, NULL, NULL );
+
+            // Draw red square
+            SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+            SDL_SetRenderDrawColor( gRenderer, 0xFF, 0x00, 0x00, 0xFF );
+            SDL_RenderFillRect( gRenderer, &fillRect );
+
+            // Draw green square (filled)
+            SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
+            SDL_SetRenderDrawColor( gRenderer, 0x00, 0xFF, 0x00, 0xFF );        
+            SDL_RenderDrawRect( gRenderer, &outlineRect );
+
+            // Draw blue horizontal line
+            SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0xFF, 0xFF );
+            SDL_RenderDrawLine( gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2 );
+
+            // Draw yellow vertical line (dashed)
+            SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0x00, 0xFF );
+            for( int i = 0; i < SCREEN_HEIGHT; i++)
+            {
+                if(i % 8 < 4)
+                {
+                    SDL_RenderDrawPoint( gRenderer, SCREEN_WIDTH / 2, i);
+                }
+            }
+
+            //Update screen
             SDL_RenderPresent( gRenderer );
         }
 
